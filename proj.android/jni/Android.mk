e840ed21e1e0a7b8a0b76f878bde4a34b98762ad
Moh.Header <mohheader@hotmail.com>
2015-01-31 13:28:46 +0200

    init Android

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -12,7 +12,19 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+				   ../../Classes/MainMenuScene.cpp \
+				   ../../Classes/GameScene.cpp \
+				   ../../Classes/Item.cpp \
+				   ../../Classes/SmallItem.cpp \
+				   ../../Classes/FruitsPage.cpp \
+				   ../../Classes/PageViewWIndicator.cpp \
+				   ../../Classes/PageIndicator.cpp \
+				   ../../Classes/PageIndicatorItem.cpp \
+				   ../../Classes/Board.cpp \
+				   ../../Classes/libs/pugixml/pugixml.cpp \
+				   ../../Classes/helpers/wordsXMLHelper.cpp \
+				   ../../Classes/helpers/arLettersUtils.cpp \
+				   ../../Classes/helpers/lettersSoundUtils.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
